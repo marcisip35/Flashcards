9baa34c594a6cd420b4bf76f07430d43dc1dec27
Marc Isip <marckevinisip@hotmail.com>
2025-11-25 13:02:25 -0600

    Added delete function in view delete a card section

--- script.js
+++ script.js
@@ -29,27 +29,27 @@ let sectionIds =
         {
             sectionBtn: sectionBtns[2],
             sectionName: sections[2].getAttribute("id")
-        }, 
+        },
         {
             sectionBtn: sectionBtns[3],
             sectionName: sections[3].getAttribute("id")
-        },        
+        },
         {
             sectionBtn: sectionBtns[4],
             sectionName: sections[4].getAttribute("id")
         }
     ];
 
-sectionBtns.forEach((section,i) => {
+sectionBtns.forEach((section, i) => {
     section.addEventListener("click", () => {
-        hideAllSectionsExcept(sectionIds[i].sectionName,i);
+        hideAllSectionsExcept(sectionIds[i].sectionName, i);
     });
 });
 
-function hideAllSectionsExcept(sectionName,index){
+function hideAllSectionsExcept(sectionName, index) {
     sections[index].style.display = "flex";
-    for(let i = 0; i < sectionIds.length; i++){
-        if (sections[i].getAttribute("id") !== sectionName){
+    for (let i = 0; i < sectionIds.length; i++) {
+        if (sections[i].getAttribute("id") !== sectionName) {
             sections[i].style.display = "none";
         }
     }
@@ -78,38 +78,132 @@ let addCardBtn = document.getElementById("add-card");
 /* Get Items From Local Storage */
 let flashcardsArray = localStorage.getItem("flashcards") ? JSON.parse(localStorage.getItem("flashcards")) : [];
 
+/* Save to Local Storage */
+function saveToLocalStorage() {
+    localStorage.setItem("flashcards", JSON.stringify(flashcardsArray));
+}
+
 /* Add A Card Event */
 addACardForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
     let newCard = {
-        id: Date.now(),
+        card_id: Date.now(),
         title: addACardTitle.value,
         definition: addACardDefinition.value
     };
 
-    /* Save to Local Storage */
     flashcardsArray.push(newCard);
-    localStorage.setItem("flashcards", JSON.stringify(flashcardsArray));
+    saveToLocalStorage();
 
     /* Clear Add Card */
     addACardTitle.value = "";
     addACardDefinition.value = "";
+
+    //Display cards function
+    displayCards();
 });
 
 /* View Cards Section */
 let viewCardsTable = document.getElementById("view-cards-table");
+let cardsTable = document.getElementsByClassName("cards-table");
 
-/* Display Cards */
-for (let i = 0; i < flashcardsArray.length; i++) {
-    let viewCardsFormat =
-        `
-    <tr id="${flashcardsArray[i].id}">
-        <td>${flashcardsArray[i].title}</td>
-        <td>${flashcardsArray[i].definition}</td>
-    </tr>
-    `;
+// Display Cards 
+function displayCards() {
+    //Cleans the tables for a fresh slate of data
+    for (let i = 0; i < cardsTable.length; i++) {
+        cardsTable[i].innerHTML =
+            `
+            <tr id="view-cards-table-title">
+                <th>Title</th>
+                <th>Definition</th>
+            </tr>
+        `;
 
-    viewCardsTable.innerHTML += viewCardsFormat;
+        //Displays all data in a table format for each table
+        for (let j = 0; j < flashcardsArray.length; j++) {
+            let tableCardsFormat;
+
+            //For view cards section
+            if (i === 0) {
+                tableCardsFormat =
+                    `
+                <tr card_id="${flashcardsArray[j].card_id}">
+                    <td style="width: 50%;">${flashcardsArray[j].title}</td>
+                    <td style="width: 50%;">${flashcardsArray[j].definition}</td>
+                </tr>
+            `;
+
+                //For edit cards section
+            } else if (i === 1) {
+                tableCardsFormat =
+                    `
+                <tr card_id="${flashcardsArray[j].card_id}">
+                    <td style="width: 45%;">${flashcardsArray[j].title}</td>
+                    <td style="width: 45%;">${flashcardsArray[j].definition}</td>
+                    <td style="width: 10%;"><button class="edit-a-card-btn" card_edit_id="${flashcardsArray[j].card_id}">Edit</button></td>
+                </tr>
+            `;
+
+                //For remove cards section
+            } else if (i === 2) {
+                tableCardsFormat =
+                    `
+                <tr card_id="${flashcardsArray[j].card_id}">
+                    <td style="width: 45%;">${flashcardsArray[j].title}</td>
+                    <td style="width: 45%;">${flashcardsArray[j].definition}</td>
+                    <td style="width: 10%;"><button class="delete-a-card-btn" card_delete_id="${flashcardsArray[j].card_id}">Delete</button></td>
+                </tr>
+            `;
+
+            } else {
+                console.log("!!!Displaying table cards error --- Corresponding table not found: ", cardsTable[i]);
+            }
+
+            cardsTable[i].innerHTML += tableCardsFormat;
+        }
+    }
 }
+displayCards();
+
+/* Remove A Card Section */
+let removeAllCards = document.getElementById("remove-all-cards");
+
+// Remove all cards
+removeAllCards.addEventListener("click", () => {
+    let removeAllConfirm = confirm("Are you sure you want to delete all cards");
+
+    if (removeAllConfirm) {
+        localStorage.removeItem("flashcards");
+        flashcardsArray = [];
+        displayCards();
+    }
+});
+
+// Remove specific card
+const deleteACardBtns = document.querySelectorAll(".delete-a-card-btn");
+deleteACardBtns.forEach((btn) => {
+    console.log("test");
+
+    btn.addEventListener("click", () => {
+        let card_delete_id = btn.getAttribute("card_delete_id");
+
+        for (let i = 0; i < flashcardsArray.length; i++) {
+            if (parseInt(card_delete_id) === parseInt(flashcardsArray[i].card_id)) {
+                flashcardsArray.splice(i, 1);
+                saveToLocalStorage();
+                break;
+            }
+        }
+    });
+});
+
+
+// Finds the card you are clicking on
+function findCard() {
+
+}
+
 
 
 
